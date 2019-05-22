f84dcc98cd3efec5d5d80d8d20c40f278c7a2036
Aditya Nerella <apnerella@gmail.com>
2019-05-22 10:58:38 -0400

    Operations, Customers, and Main classes included

---
+++ Customers.java
@@ -0,0 +1,30 @@
+public class Customers implements Operations{
+
+	public void insert(User user){
+        //Inserts the created user into mySQL database
+
+        //Log every insetion
+
+        //Exceptions from failure to insert must be handled.
+	}
+
+	public void update(int userID){
+        //Updates an existing user in the mySQL database
+
+        //Log every update
+        
+        //Exceptions from failure to update must be handled.
+	}
+
+	public void delete(int userID){
+        //Deletes an existing user in the mySQL database
+        
+        //Log every deletion
+        
+        //Exceptions from failure to delete records must be handled.
+	}
+
+	public void printAll(){
+        //Prints all the users in the mySQL database
+	}
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+
+public class Main{
+    public static void main(String [] args){
+        // Establish Database Connection using JDBC
+
+        // Write Switch statements to allow user to choose the operation to perform
+
+    }
+}
